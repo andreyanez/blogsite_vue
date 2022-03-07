68aeda15e8e1b3030466e2690056dc541ec11453
Andre Yanez <andre.yanez22@gmail.com>
2022-03-07 02:25:01 -0500

    created project, added files and raw html, icons, funcionality and swiper pending

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -18,8 +18,15 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHistory(),
+  routes,
+  scrollBehavior (savedPosition) {
+    if (savedPosition) { // <----
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
